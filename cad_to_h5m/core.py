f4aace5a5f37ce3f67a2eddab45d63b7af24d764
Jonathan Shimwell <mail@jshimwell.com>
2021-08-02 13:32:17 +0100

    started tests and docker env

---
+++ cad_to_h5m/core.py
@@ -0,0 +1,12 @@
+import sys
+
+def stp_converter(
+    input='part1.stp',
+    output='dagmc.h5m',
+    tags='mat:1',
+    cubit_path='/opt/Coreform-Cubit-2021.5/bin/python3/'
+):
+
+    sys.path.append(cubit_path)
+    import cubit
+    cubit.init([])
