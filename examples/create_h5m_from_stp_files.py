5a022339a4853ddb1ac5991e12d5c3dde45d0f83
Jonathan Shimwell <mail@jshimwell.com>
2021-08-04 00:02:57 +0100

    added tests for file production

--- examples/create_h5m_from_stp_files.py
+++ examples/create_h5m_from_stp_files.py
@@ -1,64 +1,62 @@
 
 from cad_to_h5m import cad_to_h5m
 import urllib.request
+import tarfile
 
-cd 
+url = 'https://github.com/Shimwell/fusion_example_for_openmc_using_paramak/archive/refs/tags/v0.0.1.tar.gz'
+urllib.request.urlretrieve(url, 'v0.0.1.tar.gz')
 
-# url = 'https://raw.githubusercontent.com/Shimwell/fusion_example_for_openmc_using_paramak/main/stp_files/blanket.stp'
-# urllib.request.urlretrieve(url, 'blanket.stp')
+tar = tarfile.open('v0.0.1.tar.gz', "r:gz")
+tar.extractall()
+tar.close()
 
 cad_to_h5m(
     files_with_tags=[
         {
             "material_tag": "pf_coil_mat",
-            "filename": "pf_coils.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/pf_coils.stp",
         },
         {
             "material_tag": "pf_coil_case_mat",
-            "filename": "pf_coil_cases.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/pf_coil_cases.stp",
         },
         {
             "material_tag": "center_column_shield_mat",
-            "filename": "center_column_shield.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/center_column_shield.stp",
         },
         {
             "material_tag": "firstwall_mat",
-            "filename": "outboard_firstwall.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/outboard_firstwall.stp",
         },
         {
             "material_tag": "blanket_mat",
-            "filename": "blanket.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/blanket.stp",
         },
         {
             "material_tag": "divertor_mat",
-            "filename": "divertor.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/divertor.stp",
         },
         {
             "material_tag": "supports_mat",
-            "filename": "supports.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/supports.stp",
         },
         {
             "material_tag": "blanket_rear_wall_mat",
-            "filename": "outboard_rear_blanket_wall.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/outboard_rear_blanket_wall.stp",
         },
         {
             "material_tag": "inboard_tf_coils_mat",
-            "filename": "inboard_tf_coils.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/inboard_tf_coils.stp",
         },
         {
             "material_tag": "outer_tf_coil_mat",
-            "filename": "outboard_tf_coil.stp",
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/outboard_tf_coil.stp",
         },
         {
             "material_tag": "graveyard",
-            "filename": "graveyard.stp",
-        },
-        {
-            "material_tag": "vacuum",
-            "filename": "sector_wedge.stp",
-            # "surface_reflectivity": True,
+            "filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/graveyard.stp",
         }
     ],
-    h5m_filename='dagmc.h5m',
+    h5m_filename=' "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/gmc.h5m',
     cubit_path='/opt/Coreform-Cubit-2021.5/bin/'
 )
