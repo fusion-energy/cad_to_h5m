ad4dbb72503a9ebe095c512d5ddc29da467030b0
Jonathan <mail@jshimwell.com>
2021-08-26 21:33:46 +0100

    changed filename tag to cad_filename

--- examples/create_h5m_from_paramak_reactor.py
+++ examples/create_h5m_from_paramak_reactor.py
@@ -1,29 +1,40 @@
 import paramak
+from cad_to_h5m import cad_to_h5m
 
-my_reactor = paramak.BallReactor(
-    inner_bore_radial_thickness=10,
+
+my_reactor = paramak.SubmersionTokamak(
+    inner_bore_radial_thickness=30,
     inboard_tf_leg_radial_thickness=30,
-    center_column_shield_radial_thickness=60,
-    divertor_radial_thickness=150,
-    inner_plasma_gap_radial_thickness=30,
-    plasma_radial_thickness=300,
-    outer_plasma_gap_radial_thickness=30,
+    center_column_shield_radial_thickness=30,
+    divertor_radial_thickness=80,
+    inner_plasma_gap_radial_thickness=50,
+    plasma_radial_thickness=200,
+    outer_plasma_gap_radial_thickness=50,
     firstwall_radial_thickness=30,
-    blanket_radial_thickness=50,
     blanket_rear_wall_radial_thickness=30,
-    elongation=2,
-    triangularity=0.55,
     number_of_tf_coils=16,
-    rotation_angle=90,
+    rotation_angle=360,
+    support_radial_thickness=90,
+    inboard_blanket_radial_thickness=30,
+    outboard_blanket_radial_thickness=30,
+    elongation=2.00,
+    triangularity=0.50,
     pf_coil_case_thicknesses=[10, 10, 10, 10],
     pf_coil_radial_thicknesses=[20, 50, 50, 20],
     pf_coil_vertical_thicknesses=[20, 50, 50, 20],
-    pf_coil_radial_position=[500, 575, 575, 500],
-    pf_coil_vertical_position=[300, 100, -100, -300],
+    pf_coil_radial_position=[500, 550, 550, 500],
+    pf_coil_vertical_position=[270, 100, -100, -270],
     rear_blanket_to_tf_gap=50,
-    outboard_tf_coil_radial_thickness=100,
-    outboard_tf_coil_poloidal_thickness=50,
+    outboard_tf_coil_radial_thickness=30,
+    outboard_tf_coil_poloidal_thickness=30,
 )
 
-# this function makes up of cad_to_h5m within the paramak
-my_reactor.export_h5m_with_cubit()
+my_reactor.export_stp()
+
+files_with_tags = my_reactor.neutronics_description()
+
+cad_to_h5m(
+    h5m_filename=' "dagmc.h5m',
+    cubit_path="/opt/Coreform-Cubit-2021.5/bin/",
+    files_with_tags=files_with_tags
+)
--- examples/create_h5m_from_paramak_shape.py
+++ examples/create_h5m_from_paramak_shape.py
@@ -1,4 +1,5 @@
 import paramak
+from cad_to_h5m import cad_to_h5m
 
 my_shape = paramak.Plasma(
     major_radius=620,
@@ -8,5 +9,12 @@ my_shape = paramak.Plasma(
     rotation_angle=90,
 )
 
-# this function makes up of cad_to_h5m within the paramak
-my_shape.export_h5m_with_cubit()
+my_shape.export_stp()
+
+files_with_tags = my_shape.neutronics_description()
+
+cad_to_h5m(
+    h5m_filename=' "dagmc.h5m',
+    cubit_path="/opt/Coreform-Cubit-2021.5/bin/",
+    files_with_tags=files_with_tags
+)
