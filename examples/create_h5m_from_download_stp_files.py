a8266dd44299209004d4df3e03b5e972524a8bf4
autopep8 <autopep8@users.noreply.github.com>
2021-10-13 17:12:13 +0000

    Automated autopep8 fixes

--- examples/create_h5m_from_download_stp_files.py
+++ examples/create_h5m_from_download_stp_files.py
@@ -11,54 +11,41 @@ tar = tarfile.open("v0.0.1.tar.gz", "r:gz")
 tar.extractall()
 tar.close()
 
-cad_to_h5m(
-    h5m_filename="dagmc.h5m",
-    verbose=False,
-    cubit_path="/opt/Coreform-Cubit-2021.5/bin/",
-    files_with_tags=[
-        {
-            "material_tag": "pf_coil_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/pf_coils.stp",
-        },
-        {
-            "material_tag": "pf_coil_case_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/pf_coil_cases.stp",
-        },
-        {
-            "material_tag": "center_column_shield_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/center_column_shield.stp",
-        },
-        {
-            "material_tag": "firstwall_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/outboard_firstwall.stp",
-        },
-        {
-            "material_tag": "blanket_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/blanket.stp",
-        },
-        {
-            "material_tag": "divertor_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/divertor.stp",
-        },
-        {
-            "material_tag": "supports_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/supports.stp",
-        },
-        {
-            "material_tag": "blanket_rear_wall_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/outboard_rear_blanket_wall.stp",
-        },
-        {
-            "material_tag": "inboard_tf_coils_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/inboard_tf_coils.stp",
-        },
-        {
-            "material_tag": "outer_tf_coil_mat",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/outboard_tf_coil.stp",
-        },
-        {
-            "material_tag": "graveyard",
-            "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/graveyard.stp",
-        },
-    ],
-)
+cad_to_h5m(h5m_filename="dagmc.h5m",
+           verbose=False,
+           cubit_path="/opt/Coreform-Cubit-2021.5/bin/",
+           files_with_tags=[{"material_tag": "pf_coil_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/pf_coils.stp",
+                             },
+                            {"material_tag": "pf_coil_case_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/pf_coil_cases.stp",
+                             },
+                            {"material_tag": "center_column_shield_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/center_column_shield.stp",
+                             },
+                            {"material_tag": "firstwall_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/outboard_firstwall.stp",
+                             },
+                            {"material_tag": "blanket_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/blanket.stp",
+                             },
+                            {"material_tag": "divertor_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/divertor.stp",
+                             },
+                            {"material_tag": "supports_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/supports.stp",
+                             },
+                            {"material_tag": "blanket_rear_wall_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/outboard_rear_blanket_wall.stp",
+                             },
+                            {"material_tag": "inboard_tf_coils_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/inboard_tf_coils.stp",
+                             },
+                            {"material_tag": "outer_tf_coil_mat",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/outboard_tf_coil.stp",
+                             },
+                            {"material_tag": "graveyard",
+                             "cad_filename": "fusion_example_for_openmc_using_paramak-0.0.1/stp_files/graveyard.stp",
+                             },
+                            ],
+           )
